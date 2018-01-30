80841ea5288361b6d9c70bd4ff57d00308f9f445
Nicolas Clauvelin <n.clauvelin+code@gmail.com>
2018-01-29 22:56:42 -0500

    REVISED FLAGS FOR CLANG ON MAC OS
    Linker flags added separately from the compiler flags.

--- scripts/compilers_support/OSX_Clang.cmake
+++ scripts/compilers_support/OSX_Clang.cmake
@@ -2,16 +2,19 @@
 # CMake Hephaistos::CompilerSetup OSX Clang compiler flags
 #
 # Nicolas Clauvelin (n.clauvelin+code@gmail.com)
-# nicocvn.com, 2017
+# nicocvn.com, 2018
 #
 #
 # DESCRIPTION:
 #
-#   Regular flags with some added warnings for debug builds.
-#   Address sanitizer enabled for debug builds.
-#   C++14 disabled.
-#   MinSizeRel relies on LTO and OS X dead strip to minimize binary size.
-#   For MinSizeRel RTTI is disabled.
+#   * C99 and C++11 enforced.
+#   * For all build types visibility is set to hidden.
+#   * LTO and dead code stripping are enabled for Release and MinSizeRel only
+#     (for RelWithDebInfo: it is not recommended to enable debug symbols with
+#     LTO)
+#   * Release and RelWithDebInfo use O2.
+#   * Release, RelWithDebInfo, and MinSizeRel use march=native and mfpmath=sse.
+#   * For Debug, Wall, Wextra, Weff-c++ and a few others are enabled.
 #
 # ---------------------------------------------------------------------------- #
 
@@ -33,14 +36,16 @@ set(C_DEBUG_FLAGS
     -Wextra
     -Wunused-value
     -Wunused
+    -Wunused-parameter
+    -Wunused-result
     -Wmissing-declarations
     -Wmissing-include-dirs
-    -Wno-unused-parameter
     -Wuninitialized
     -Wconversion
     -fsanitize=address
     -fno-omit-frame-pointer
-    -fexceptions)
+    -fexceptions
+    -Werror=return-type)
 
 # C++ debug flags.
 set(CXX_DEBUG_FLAGS
@@ -50,14 +55,19 @@ set(CXX_DEBUG_FLAGS
     -Wextra
     -Wunused-value
     -Wunused
+    -Wunused-parameter
+    -Wunused-result
     -Wmissing-declarations
     -Wmissing-include-dirs
-    -Wno-unused-parameter
     -Wuninitialized
     -Wconversion
     -fsanitize=address
     -fno-omit-frame-pointer
-    -Weffc++)
+    -Weffc++
+    -Werror=return-type)
+
+# Linker flags.
+set(LINKER_DEBUG_FLAGS )
 
 
 # --- Release flags ---
@@ -68,6 +78,8 @@ set(C_RELEASE_FLAGS
     -O2
     -DNDEBUG
     -flto
+    -ffunction-sections
+    -fdata-sections
     -march=native
     -mfpmath=sse
     -fexceptions)
@@ -78,8 +90,14 @@ set(CXX_RELEASE_FLAGS
     -O2
     -DNDEBUG
     -flto
+    -ffunction-sections
+    -fdata-sections
     -march=native
     -mfpmath=sse)
+
+# Release linker flags.
+set(LINKER_RELEASE_FLAGS
+    -dead_strip)
 
 
 # --- RelWithDebInfo flags ---
@@ -88,7 +106,7 @@ set(CXX_RELEASE_FLAGS
 set(C_RELWITHDEBINFO_FLAGS
     -fvisibility=hidden
     -O2
-    -g
+    -g3
     -DNDEBUG
     -march=native
     -mfpmath=sse
@@ -98,10 +116,14 @@ set(C_RELWITHDEBINFO_FLAGS
 set(CXX_RELWITHDEBINFO_FLAGS
     -fvisibility=hidden
     -O2
-    -g
+    -g3
     -DNDEBUG
     -march=native
     -mfpmath=sse)
+
+# Release with debug info linker flags.
+set(LINKER_RELWITHDEBINFO_FLAGS
+    -dead_strip)
 
 
 # --- MinSizeRel flags ---
@@ -119,9 +141,7 @@ set(C_MINSIZEREL_FLAGS
     -fexceptions
     -flto
     -ffunction-sections
-    -fdata-sections
-    -Qunused-arguments
-    -Wl,-dead_strip)
+    -fdata-sections)
 
 # C++ minimal size release flags.
 set(CXX_MINSIZEREL_FLAGS
@@ -133,9 +153,11 @@ set(CXX_MINSIZEREL_FLAGS
     -fno-rtti
     -flto
     -ffunction-sections
-    -fdata-sections
-    -Qunused-arguments
-    -Wl,-dead_strip)
+    -fdata-sections)
+
+# Minimal size release linker flags.
+set(LINKER_MINSIZEREL_FLAGS
+    -dead_strip)
 
 
 # --- Compiler flags setup ---
@@ -143,33 +165,45 @@ set(CXX_MINSIZEREL_FLAGS
 # We start by replacing the previously defined lists with string.
 string(REPLACE ";" " " C_DEBUG_FLAGS "${C_DEBUG_FLAGS}")
 string(REPLACE ";" " " CXX_DEBUG_FLAGS "${CXX_DEBUG_FLAGS}")
+string(REPLACE ";" " " LINKER_DEBUG_FLAGS "${LINKER_DEBUG_FLAGS}")
 #
 string(REPLACE ";" " " C_RELEASE_FLAGS "${C_RELEASE_FLAGS}")
 string(REPLACE ";" " " CXX_RELEASE_FLAGS "${CXX_RELEASE_FLAGS}")
+string(REPLACE ";" " " LINKER_RELEASE_FLAGS "${LINKER_RELEASE_FLAGS}")
 #
 string(REPLACE ";" " " C_RELWITHDEBINFO_FLAGS "${C_RELWITHDEBINFO_FLAGS}")
 string(REPLACE ";" " " CXX_RELWITHDEBINFO_FLAGS "${CXX_RELWITHDEBINFO_FLAGS}")
+string(REPLACE ";" " " LINKER_RELWITHDEBINFO_FLAGS "${LINKER_RELWITHDEBINFO_FLAGS}")
 #
 string(REPLACE ";" " " C_MINSIZEREL_FLAGS "${C_MINSIZEREL_FLAGS}")
 string(REPLACE ";" " " CXX_MINSIZEREL_FLAGS "${CXX_MINSIZEREL_FLAGS}")
+string(REPLACE ";" " " LINKER_MINSIZEREL_FLAGS "${LINKER_MINSIZEREL_FLAGS}")
 
 # Set CMake flags.
 set(CMAKE_C_FLAGS_DEBUG "${C_DEBUG_FLAGS}"
     CACHE STRING "Debug C flags" FORCE)
 set(CMAKE_CXX_FLAGS_DEBUG "${CXX_DEBUG_FLAGS}"
     CACHE STRING "Debug C++ flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${LINKER_DEBUG_FLAGS}"
+    CACHE STRING "Debug linker flags" FORCE)
 #
 set(CMAKE_C_FLAGS_RELEASE "${C_RELEASE_FLAGS}"
     CACHE STRING "Release C flags" FORCE)
 set(CMAKE_CXX_FLAGS_RELEASE "${CXX_RELEASE_FLAGS}"
     CACHE STRING "Release C++ flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${LINKER_RELEASE_FLAGS}"
+    CACHE STRING "Release linker flags" FORCE)
 #
 set(CMAKE_C_FLAGS_RELWITHDEBINFO "${C_RELWITHDEBINFO_FLAGS}"
     CACHE STRING "RelWithDebInfo C flags" FORCE)
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CXX_RELWITHDEBINFO_FLAGS}"
     CACHE STRING "RelWithDebInfo C++ flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "${LINKER_RELWITHDEBINFO_FLAGS}"
+    CACHE STRING "RelWithDebInfo linker flags" FORCE)
 #
 set(CMAKE_C_FLAGS_MINSIZEREL "${C_MINSIZEREL_FLAGS}"
     CACHE STRING "MinSizeRel C flags" FORCE)
 set(CMAKE_CXX_FLAGS_MINSIZEREL "${CXX_MINSIZEREL_FLAGS}"
     CACHE STRING "MinSizeRel C++ flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL "${LINKER_MINSIZEREL_FLAGS}"
+    CACHE STRING "MinSizeRel linker flags" FORCE)
